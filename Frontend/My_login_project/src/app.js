bae472348b02ad8b550b78a996db4778704d92f7
jack2025-star <lkx2006.09@qq.com>
2026-05-16 18:46:14 +0800

    20260516

--- Frontend/My_login_project/src/app.js
+++ Frontend/My_login_project/src/app.js
@@ -1,26 +1,44 @@
+// 引入 Node.js 内置模块
+const path = require('path');
 //引入Express
 const express = require('express');
-
+const cors = require('cors');
 
 // 创建应用实例
 const app = express();
 
+// 解析JSON格式的请求体
+app.use(express.json());
+app.use(cors());
 
-// 根路径
+// 配置静态文件服务：将 src 目录设为静态资源根目录
+app.use(express.static(path.join(__dirname)));
+
+//模拟数据库中的用户数据
+const users = [
+    { username: 'admin', password: '123456' }
+]
+
+// 首页路由：直接返回登录页面（可选，若静态文件已托管可省略）
 app.get('/', (req, res) => {
-    res.send('欢迎来到首页！！');
+    res.sendFile(path.join(__dirname, 'login.html'));
 });
 
-//about路径
-app.get('/about', (req, res) => {
-    res.send('这是一个关于页面')
-});
 
-//usr路径
-app.get('/user', (req, res) => {
-    const user = { name: "小明", age: 20 };
-    res.json(user);
-});
+// 登录路由
+app.post('/login', (req, res) => {
+    // 从请求体中获取用户名和密码
+    const { username, password } = req.body;
+    // 查找用户是否匹配
+    const user = users.find(u => u.username === username && u.password === password);
+
+    // 判断用户是否存在
+    if (user) {
+        res.json({ success: true, message: "登录成功！" });
+    } else {
+        res.json({ success: false, message: "用户名或密码错误！" })
+    }
+})
 
 // 启动服务器
 app.listen(3000, () => {
